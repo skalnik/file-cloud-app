8dd1197f346a8fe61f6ce06652d45719eb0af65c
Mike Skalnik <mike@skalnik.com>
2022-05-10 12:13:06 -0700

    Initial Commit

---
+++ Shared/File_CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_CloudApp.swift
+//  Shared
+//
+//  Created by Mike Skalnik on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct File_CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
