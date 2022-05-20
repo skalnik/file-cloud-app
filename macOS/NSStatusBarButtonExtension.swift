1417fb8e90f173005c6cd591fcd749205893e660
Mike Skalnik <mike@skalnik.com>
2022-05-20 12:53:46 -0700

    Extract NSStatusBarButton extension 👽️

---
+++ macOS/NSStatusBarButtonExtension.swift
@@ -0,0 +1,36 @@
+//
+//  NSStatusBarButtonExtension.swift
+//  File Cloud (macOS)
+//
+//  Created by Mike Skalnik on 5/20/22.
+//
+
+import AppKit
+
+extension NSStatusBarButton {
+    open override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        NSApp.sendAction(#selector(AppDelegate.dragEntered(_:)), to: nil, from: sender)
+        
+        return .copy
+    }
+
+    open override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        NSApp.sendAction(#selector(AppDelegate.prepareDrag(_:)), to: nil, from: sender)
+        
+        if NSURL.init(from: sender.draggingPasteboard)?.standardized != nil {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    open override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        NSApp.sendAction(#selector(AppDelegate.performDrag(_:)), to: nil, from: sender)
+
+        return true
+    }
+    
+    open override func draggingExited(_ sender: NSDraggingInfo?) {
+        NSApp.sendAction(#selector(AppDelegate.dragExit(_:)), to: nil, from: nil)
+    }
+}
