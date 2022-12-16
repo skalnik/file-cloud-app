0020bec3ea26f4983c1fcc5f51886b14592ac677
Mike Skalnik <mike@skalnik.com>
2022-12-16 10:49:06 -0800

    📱

--- iOS/MainView.swift
+++ iOS/MainView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct MainView: View {
-    @AppStorage("serverURL") var serverURL: String = ""
-    @AppStorage("username") var username: String = ""
-    @AppStorage("password") var password: String = ""
+    @State private var selection: Tab = .upload
+    
+    enum Tab {
+        case upload
+        case settings
+    }
+    
+    init() {
+        UITabBar.appearance().isTranslucent = false
+    }
     
     var body: some View {
-        Form {
-            TextField("Server URL", text: $serverURL, prompt: Text("https://cloud.example.com"))
-            TextField("Username", text: $username)
-            SecureField("Password", text: $password)
-        }
+        TabView(selection: $selection) {
+            UploadView()
+                .tabItem {
+                    Label("Upload", systemImage: "arrow.up.doc")
+                }
+                .tag(Tab.upload)
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape")
+                }
+                .tag(Tab.settings)
+        }.background(Color.white)
     }
 }
 
