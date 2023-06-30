68a26e7d4ef7be74678d2d92067ff2cdd47c4dac
Mike Skalnik <mike@skalnik.com>
2023-06-30 11:48:54 -0700

    Try to use the new SwiftUI MenuBarExtra 😤

--- Shared/File_CloudApp.swift
+++ Shared/File_CloudApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 @main
 struct File_CloudApp: App {
@@ -13,12 +14,43 @@ struct File_CloudApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     #endif
     
+    struct MyDropDelegate: DropDelegate {
+        func dropEntered(info: DropInfo) {
+            return
+        }
+        func dropExited(info: DropInfo) {
+        }
+        func dropUpdated(info: DropInfo) -> DropProposal? {
+            return nil
+        }
+        func performDrop(info: DropInfo) -> Bool {
+            return true
+        }
+    }
+    
     
     var body: some Scene {
 #if os(macOS)
         Settings {
             SettingsView()
         }
+        
+        @State var colorIndex = 0
+        @State var isInserted = true
+        let dropDelegate = MyDropDelegate()
+        
+        MenuBarExtra(content: {
+            Text("📁☁️ File Cloud")
+            Divider()
+            Button("Preferences…") {
+                NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
+            }.keyboardShortcut(",")
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
+        }, label: {
+            Text("Butt").onDrop(of: [.fileURL], delegate: dropDelegate)
+        })
 #endif
 #if os(iOS)
         WindowGroup {
