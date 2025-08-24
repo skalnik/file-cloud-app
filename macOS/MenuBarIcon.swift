0b7788ec387f245a08e9dfd3b87ead432a7421dc
Mike Skalnik <mike@skalnik.com>
2025-08-23 21:17:07 -0700

    Make the settings window show up on top 📚

--- macOS/MenuBarIcon.swift
+++ macOS/MenuBarIcon.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 import MenuBarExtraAccess
+import AppKit
+import Combine
 
 struct MenuBarIcon: Scene {
     @State var isMenuPresented: Bool = false
@@ -19,7 +21,25 @@ struct MainMenu: View {
     var body: some View {
         Text("📂☁️ File Cloud")
         Divider()
-        SettingsLink().keyboardShortcut(",", modifiers: .command)
+        SettingsLink()
+            .keyboardShortcut(",", modifiers: .command)
+            .modifier(SettingsWindowActivator())
         Button("Quit") { NSApplication.shared.terminate(nil) }.keyboardShortcut("q", modifiers: .command)
+    }
+}
+
+struct SettingsWindowActivator: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                NSApp.activate(ignoringOtherApps: true)
+                for window in NSApplication.shared.windows {
+                    if window.title == "Settings" {
+                        window.makeKeyAndOrderFront(nil)
+                        return
+                    }
+                }
+                NSApp.sendAction(Selector(("showPreferencesWindow:")), to: nil, from: nil)
+            }
     }
 }
